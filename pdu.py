dca871bfe9236921386269e0cb26737dc2721853
Mark Francis <jamiitech1@gmail.com>
2016-04-27 13:00:41 +0300

    Update pdu.py

--- pdu.py
+++ pdu.py
@@ -2,8 +2,8 @@ from python-setuptools import pdu
 from python-setuptools import os
 
 
-def (feb /a?*r==s)',
+def (feb /a*r==s)',
  a int( 
-                 
+     r = ?             
                  
           )',
